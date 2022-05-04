8e9d5a38df4fdffd238d19c0c8fc1531dda52aa7
zelleous <80587691+zelleous@users.noreply.github.com>
2022-05-04 18:59:19 +0800

    Add files via upload

---
+++ api/UserController/UpdateUser.php
@@ -0,0 +1,46 @@
+<?php
+    class UpdateUser{
+        public function __construct()
+        {
+            $this->run();
+        }
+
+        public function run(){
+            header('Access-Control-Allow-Origins: *');
+            header ('Content-Type: application/x-www-form-urlencoded');
+            header('Access-Control-Allow-Methods: PUT');
+            header('Access-Control-Allow-Header: Accecs-Control-Allow-Headers,Access-Control-Allow-Methods,Content-Type, Authorization');
+
+            include_once './config/Database.php';
+            include_once './models/Forms.php';
+
+            $databse = new Database();
+            $db = $databse->connect();
+
+            $forms = new UserForms($db);
+
+            $data = json_decode(file_get_contents('php://input'));
+
+            $forms->first_name = $data->first_name;
+            $forms->middle_name = $data->middle_name;
+            $forms->last_name = $data->last_name;
+            $forms->birthday = $data->birthday;
+            $forms->suffix = $data->suffix;
+            $forms->gender = $data->gender;
+            $forms->email = $data->email;
+            $forms->mobile_number = $data->mobile_number;
+            $forms->contact_person = $data->contact_person;
+            $forms->contact_person_number = $data->contact_person_number;
+            $forms->password = $data->password;
+
+            if ($forms->createUser()){
+                echo json_encode(array(
+                    'message' => 'SUCCESS'
+                ));
+            }else{
+                echo json_encode(array(
+                    'message' => 'FAILED'
+                ));
+            }
+        }
+    }
